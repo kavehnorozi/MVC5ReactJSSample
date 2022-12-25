e7f420c42dc5fb712707101c950c4b0795347d41
kaveh norouzi <kaveh.norozi@live.com>
2022-12-25 14:10:28 +0330

    Add project files.

---
+++ ReactSample/Scripts/Tutorial.jsx
@@ -0,0 +1,29 @@
+﻿class CommentBox extends React.Component {
+    render() {
+        return (
+            <ul id="nav">
+                <li><a href="#">Home</a></li>
+                <li><a href="#">About</a></li>
+                <li><a href="#">blog</a></li>
+                <li><a href="#">Contact Us</a></li>
+            </ul>
+        );
+    }
+}
+class CommentList extends React.Component {
+    render() {
+        return (
+            <div className="commentBox">
+                <h1>Comments</h1>
+                <CommentList />
+                <CommentForm />
+            </div>
+        );
+    }
+}
+
+ReactDOM.render(
+    React.createElement(CommentBox, null),
+    document.getElementById('content'),
+
+);
